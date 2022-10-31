f6ad665a2000fa19982e386d64c3cd8093e83756
Daniel Calvi <dcalvi@sweatworks.net>
2022-10-31 10:03:22 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import morgan from "morgan";
+import { Server as SocketServer } from "socket.io";
+import http from "http";
+import cors from "cors";
+import { PORT } from "./config.js";
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+app.use(cors());
+app.use(morgan("dev"));
+
+io.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    console.log(message);
+    socket.broadcast.emit("message", {
+      body: message,
+      from: socket.id,
+    });
+  });
+});
+
+app.use(express.static("public"));
+
+server.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
